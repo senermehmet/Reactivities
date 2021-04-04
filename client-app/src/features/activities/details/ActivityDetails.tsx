554c1eb8fe10963ab69f72e9aadb51eb6a0c0af5
senermehmet <mehmet.kim@gmail.com>
2021-04-05 00:25:43 +0300

    Line 12:25:  Unexpected template string expression no-template-curly-in-string

---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Card, Image} from 'semantic-ui-react';
+import { Activity } from '../../../app/models/activity';
+
+interface Props {
+    activity : Activity
+}
+
+export default function ActivityDetails({activity}: Props) {
+    return (
+        <Card>
+            <Image src={'/asset/categoryImages/${activity.category}.jpg'} />
+            <Card.Content>
+                <Card.Header>{activity.title}</Card.Header>
+                <Card.Meta>
+                    <span >{activity.date}</span>
+                </Card.Meta>
+                <Card.Description>
+                    {activity.description}
+      </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+                <Button.Group winds='2'>
+                    <Button basic color='blue' content='Edit'/>
+                    <Button basic color='grey' content='Cancel'/>
+                </Button.Group>
+            </Card.Content>
+        </Card>
+    )
+}
