db62d0903bca1521fb512b3800117baec928eadf
senermehmet <mehmet.kim@gmail.com>
2021-05-01 13:40:29 +0300

    End of Section 7 Mobx and AutoObservable

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Container } from 'semantic-ui-react';
-import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDasboard from '../../features/activities/dashboard/ActivityDashboard';
-import { v4 as uuid } from 'uuid';
-import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
 import { observer } from 'mobx-react-lite';
@@ -13,45 +10,10 @@ import { observer } from 'mobx-react-lite';
 function App() {
 
   const { activityStore } = useStore();
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
-
 
   useEffect(() => {
     activityStore.loadActivities();
   }, [activityStore])
-
-  function hendleCreateOrEditActivity(activity: Activity) {
-    setSubmitting(true);
-    if (activity.id) {
-      agent.Activities.update(activity).then(() => {
-        setActivities([...activities.filter(x => x.id !== activity.id), activity])
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-    else {
-      activity.id = uuid();
-      agent.Activities.create(activity).then(() => {
-        setActivities([...activities, activity]);
-        setSelectedActivity(activity);
-        setEditMode(false);
-        setSubmitting(false);
-      })
-    }
-  }
-
-  function handleDeleteActivity(id: string) {
-    setSubmitting(true);
-    agent.Activities.delete(id).then(() => {
-      setActivities([...activities.filter(x => x.id !== id)]);
-      setSubmitting(false);
-    })
-
-  }
 
   if (activityStore.loadingInitial) return <LoadingComponent content='Loading App...' />
 
@@ -59,12 +21,7 @@ function App() {
     <>
       <NavBar />
       <Container style={{ marginTop: '7em' }}>
-        <ActivityDasboard
-          activities={activityStore.activities}
-          createOrEdit={hendleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-          submitting={submitting}
-        />
+        <ActivityDasboard />
       </Container>
     </>
   );
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,39 +1,24 @@
 import { observer } from 'mobx-react-lite';
 import { Grid } from 'semantic-ui-react';
-import { Activity } from '../../../app/models/activity';
 import { useStore } from '../../../app/stores/store';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivitiyForm from '../form/ActivityForm';
 import ActivityList from './ActivityList';
 
-interface Props {
-    activities: Activity[];
-
-    createOrEdit: (activity: Activity) => void;
-    deleteActivity: (id: string) => void;
-    submitting: boolean;
-}
-
 export default observer(
-    function ActivityDasboard({ activities, createOrEdit, deleteActivity, submitting }: Props) {
+    function ActivityDasboard() {
         const { activityStore } = useStore();
         const { selectedActivity, editMode } = activityStore;
         return (
             <Grid>
                 <Grid.Column width='10'>
-                    <ActivityList activities={activities}
-                        deleteActivity={deleteActivity}
-                        submitting={submitting}
-                    />
+                    <ActivityList />
                 </Grid.Column>
                 <Grid.Column width='6'>
                     {selectedActivity &&
                         <ActivityDetails />}
                     {editMode &&
-                        <ActivitiyForm
-                            createOrEdit={createOrEdit}
-                            submitting={submitting}
-                        />}
+                        <ActivitiyForm />}
                 </Grid.Column>
             </Grid>
         )
